4cfa094dbd97abf9e901acf6d51a921a532a18ae
Alexander Tatevosyan <mangaka585@gmail.com>
2023-07-26 12:13:00 +0300

    cycle lesson 1

---
+++ cycle/1.cpp
@@ -0,0 +1,25 @@
+/*
+    Напишите программу, которая выводит квадраты нечетных целых чисел от 1 до предела который вводит пользователь.
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int a = 0;
+
+    cout << "Введите любое целое число" << endl;
+    cin >> a;
+
+    for (int i = 0; i <= a; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i * i << endl;
+        }
+    }
+}
